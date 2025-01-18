72015df6af1b60f49766091a30307a3b0614b8f4
Dragon_Fly <143097451+gurpreetsinghwazir@users.noreply.github.com>
2025-01-19 01:03:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 let search = document.querySelector(".input");
 let btn = document.querySelector(".btn");
-
+let outer = document.querySelector(".search");
 btn.addEventListener("click", show);
 
 
 
-
+ 
 function show() {
-    if (search) {
-        search.classList.toggle("active"); // Remove the dot before "hide"
+    if (search && outer) {
+        search.classList.toggle("hide"); // Remove the dot before "hide"
         search.focus();
+		outer.classList.toggle("active"); 
     }
 }
 
